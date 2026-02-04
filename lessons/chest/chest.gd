edef985d77c6802da127e433286ed167a143e135
AntheaHo0 <Anho4@s.sfusd.edu>
2026-02-03 21:18:02 -0800

    M4L5 (Completed)

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -17,3 +28,7 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+
+func open() -> void:
+	animation_player.play("Open")
