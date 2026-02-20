1c1ed57734a7fb1a8293528f4a61165afd4c0bc8
AntheaHo0 <Anho4@s.sfusd.edu>
2026-02-20 01:46:30 -0800

    M6L9 (Completed)
    Attempt was made on bird but wasn't successful. Will try on Friday.

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -74,4 +74,4 @@ func _spawn_random_item() -> void:
 	var jump_height := randf_range(30.0, 80.0)
 	tween.tween_property(loot_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
 	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME) 
